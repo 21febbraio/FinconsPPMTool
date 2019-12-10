63380e0a50a4bc1bbed309120908bfed3fa1b770
Domenico Sacino <domenico.sacino@finconsgroup.local>
2019-12-10 12:23:14 +0100

    first commit

---
+++ ppmtool/src/test/java/com/fincons/ppm/ppmtool/PpmtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fincons.ppm.ppmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmtoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
